c49aa8cdec47cbfd06619f394742e085e66dbb3c
Stephen Mayeux <stephenmayeux@gmail.com>
2016-02-05 18:57:14 +0900

    can validate URL with valid-url package

--- routes/create.js
+++ routes/create.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var URL = require('../models/url');
+var validUrl = require('valid-url');
 
 router.get('/*?', function(req, res, next) {
-  res.send(req.params[0]);
+  if (validUrl.isUri(req.params[0])) {
+    res.send('This is a valid URL');
+  } else {
+    res.send('This is not a valid URL');
+  }
 });
 
 module.exports = router;
